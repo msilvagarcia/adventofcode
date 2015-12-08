92380aa9380d5de8e7b8bcf2694d52d29dc533a7
Marcos Garcia <marcos.asgarcia@gmail.com>
2015-12-08 21:37:05 +0100

    The first time Santa reaches -1

---
+++ day1-2.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+
+fs.readFile('./day1.txt', 'utf8', function (err, data) {
+	if (err) throw err
+
+	var floor = 0
+
+	for (var c = 0; c < data.length; c++) {
+		if (data[c] === '(') floor += 1
+		if (data[c] === ')') floor -= 1
+
+		if (floor === -1) {
+			console.log(c + 1)
+			return
+		}
+	}
+	console.log(floor)
+})
